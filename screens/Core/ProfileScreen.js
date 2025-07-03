afeab33b859adbeed5432f1b4671fd6513054896
paulfersi <paolfersi@gmail.com>
2025-07-03 19:37:41 +0200

    add logout button

--- screens/Core/ProfileScreen.js
+++ screens/Core/ProfileScreen.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import { supabase } from '../../lib/supabase';
 
 export default function ProfileScreen() {
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>User Profile Info</Text>
-      <Text>Reviews by user</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile screen</Text>
+      <Button title="Logout" onPress={handleLogout} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
